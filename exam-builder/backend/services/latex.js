3d911ff9f21af09cf1b73b89b3b701b21435a9eb
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2025-03-16 13:12:37 +0000

    questions are formatted correctly on different pages, error: odd question should be by itself

--- exam-builder/backend/services/latex.js
+++ exam-builder/backend/services/latex.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const path = require("path")
 
+//This function formats the question in a consistent way
 function formatQuestion(questionNumber, questionContent, totalMarks) {
   return `
 %question ${questionNumber}
@@ -15,47 +16,83 @@ ${questionContent}
 `
 }
 
+//This function formats the pages consistently
+function formatPage(questionPair) {
+  return `
+\\begin{tcolorbox}
+[width=\\textwidth, height=\\textheight, colframe=black, colback=white, arc=5mm, boxrule=0.2mm]
+
+${questionPair}
+
+
+\\end{tcolorbox} % Close the bordered box
+`
+}
+
+//Questions comes from server
+function createQuestions(questions) {
+  const questionsContent = questions.map((question, index) => {
+    return formatQuestion(index + 1, question.content, question.totalMarks)
+  })
+
+  // console.log("successfully formatted questions")
+
+  return questionsContent
+}
+
+const groupQuestionsIntoPairs = (formattedQuestions) => {
+  const pairs = []
+  for (let i = 0; i < formattedQuestions.length; i += 2) {
+    if (i + 1 < formattedQuestions.length) {
+      // If we have two questions, pair them
+      pairs.push(formattedQuestions[i] + "\n\n" + formattedQuestions[i + 1])
+    } else {
+      // If we have an odd number of questions, the last one goes alone
+      pairs.push(formattedQuestions[i])
+    }
+  }
+  console.log("successfully paired questions", String(pairs).slice(0, 700))
+  return pairs
+}
+
+function createPages(questionPair) {
+  const finalExam = questionPair
+    .map((question) => {
+      return formatPage(questionPair)
+    })
+    .join("\n")
+  // console.log(
+  //   "successfully created exam pages",
+  //   String(finalExam).slice(0, 700)
+  // )
+  return finalExam
+}
+
 function generateExam(questions) {
+  const formattedQuestions = createQuestions(questions)
+
+  console.log(`Formatted ${formattedQuestions.length} questions`)
+
+  const questionPairs = groupQuestionsIntoPairs(formattedQuestions)
+
+  console.log(`Created ${questionPairs.length} pages`)
+
   try {
     // Read the template
     const templatePath = path.join(__dirname, "../templates/exam-template.tex")
     let template = fs.readFileSync(templatePath, "utf8")
 
     // Generate all questions content
-    const questionsContent = questions
-      .map((question, index) => {
-        return formatQuestion(index + 1, question.content, question.totalMarks)
-      })
-      .join("\n")
+    const finalExam = createPages(questionPairs)
 
     // Replace the placeholder with actual questions
-    const finalLatex = template.replace(
-      "%QUESTIONS_PLACEHOLDER%",
-      questionsContent
-    )
+    const finalLatex = template.replace("%PAGES_PLACEHOLDER", finalExam)
 
     return finalLatex
   } catch (error) {
     console.error("Error generating LaTeX:", error)
     throw error
   }
-}
-
-// Example usage:
-const exampleQuestion = {
-  content: `\\[f(x) = (x + 3)(x + 2)(x - 1)\\] 
-
-\\begin{enumerate}[label=(\\alph*)]
-\\item Sketch the curve \\( y = f(x) \\), showing the points of intersection with the coordinate axes. \\hfill \\textbf{(3)}
-\\item Showing the coordinates of the points of intersection with the coordinate axes, sketch on separate diagrams the curves:
-    \\begin{enumerate}[label=(\\roman*)]
-    \\item \\( y = f(x - 3) \\) \\hfill \\textbf{(2)}
-    \\item \\( y = f(-x) \\) \\hfill \\textbf{(2)}
-    \\end{enumerate}
-\\end{enumerate}
-
-\\vspace{8cm}`,
-  totalMarks: 7,
 }
 
 module.exports = {
