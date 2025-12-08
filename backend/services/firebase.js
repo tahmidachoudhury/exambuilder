1d9c3157a392c7719b7aa48bd19b83a6ae92f04c
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2025-12-08 15:38:09 +0000

    add: test for backend crash

--- backend/services/firebase.js
+++ backend/services/firebase.js
@@ -4,13 +4,6 @@ require("dotenv").config();
 //parse firebase service from the .env, will need to be an env variable on DO
 const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
 
-if (!serviceAccountJson) {
-  throw new Error(
-    "FIREBASE_SERVICE_ACCOUNT env var is missing. " +
-      "Set it to the JSON service account string in docker-compose or your environment."
-  );
-}
-
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
